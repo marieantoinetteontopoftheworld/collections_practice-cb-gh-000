295b70684f376492103dd50005651aa11022a864
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-05 08:54:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,16 @@ def sort_array_asc(arr)
   end
   arr
 end
+
+
+def sort_array_asc2(arr)
+  arr.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end      
+  end
+end
